cf3b9c9230ce9c1a7c86458eb2f5f6cd1e9ef8d7
ArpitTripathi <tarpit501@gmail.com>
2026-03-18 20:44:44 +0530

    add card section

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,13 +1,48 @@
 import "./card.scss";
+import { Link } from "react-router-dom";
+import address from "../../assets/pin.png";
+import bed from "../../assets/bed.png";
+import bath from "../../assets/bath.png";
+import save from "../../assets/save.png";
+import chat from "../../assets/chat.png";
 
-function Card() {
+function Card({ item }) {
   return (
     <div className="card">
-      <Link to={`${item.id}`} className="imageContainer">
-        <img src={item.image} alt="" />
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt={item.title} />
       </Link>
 
-      <div className="textContainer"></div>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src={address} alt="address" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">{item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src={bed} alt="bedroom" />
+              <span>{item.bedroom} Bedroom</span>
+            </div>
+            <div className="feature">
+              <img src={bath} alt="bathroom" />
+              <span>{item.bathroom} Bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src={save} alt="save" />
+            </div>
+            <div className="icon">
+              <img src={chat} alt="chat" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { listData } from '../../lib/dummyData'
-import './listPage.scss'
-import Filter from '../../components/filter/filter';
-import Card from '../../components/card/card';
+import React from "react";
+import { listData } from "../../lib/dummyData";
+import "./listPage.scss";
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
 
 function ListPage() {
-    const data =listData;
+  const data = listData;
   return (
-    <div className='listPage'>
-     <div className="listContainer">
-     <div className="wrapper">
-        <Filter/>
-        {data.map(item=>(
-            <Card key={item.id} item={item}/>
-        ))}
-     </div>
-     </div>
-     <div className="mapContainer">Map</div>
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
     </div>
-  )
+  );
 }
 
-export default ListPage
+export default ListPage;
